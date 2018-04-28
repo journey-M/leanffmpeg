94bb405fc7e0384c3bdb7ae0d1bc26b080ab5dee
guowj <1485765145@qq.com>
2018-04-28 18:18:56 +0800

    添加 libyuv解码

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,11 @@
 #include <jni.h>
 #include <string>
 #include "native_log.h"
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <unistd.h>
+#include "libyuv.h"
+
 
 extern "C"{
 #include "libavcodec/avcodec.h"
@@ -97,16 +102,12 @@ Java_gwj_dev_ffmpeg_MainActivity_decdoe(JNIEnv *env, jobject instance, jstring i
     struct SwsContext *sws_ctx = sws_getContext(pCodecContxt->width,pCodecContxt->height,pCodecContxt->pix_fmt,
                                                 pCodecContxt->width, pCodecContxt->height, AV_PIX_FMT_YUV420P,
                                                 SWS_BICUBIC, NULL, NULL, NULL);
-
-
     int frame_count = 0;
     int got_picture, ret;
     FILE *fp_yuv = fopen(output, "wb+");
-
     //6.一帧一帧的读取压缩数据
     while (av_read_frame(pFormateContext, packet) >= 0)
     {
-
         //是视频的 数据
         if(packet->stream_index == v_stream_idx){
             ret = avcodec_decode_video2(pCodecContxt,pFram,  &got_picture, packet);
@@ -139,23 +140,131 @@ Java_gwj_dev_ffmpeg_MainActivity_decdoe(JNIEnv *env, jobject instance, jstring i
                 frame_count++;
                 FFLOGE("解码第%d帧",frame_count);
             }
-
         }
+    }
+    FFLOGE("解码完成");
+    fclose(fp_yuv);
+    av_frame_free(&pFram);
+    avcodec_close(pCodecContxt);
+    avformat_free_context(pFormateContext);
+    env->ReleaseStringUTFChars(input_, input);
+    env->ReleaseStringUTFChars(output_, output);
+}
 
+extern "C"
+JNIEXPORT void
+JNICALL
+Java_gwj_dev_ffmpeg_MainActivity_playVideo(JNIEnv *env, jobject instance, jstring input_,
+            jobject surface) {
 
+    const char *input = env->GetStringUTFChars(input_, 0);
+    FFLOGE("输入路径 = %s",input);
+    av_register_all();
+    AVFormatContext *pFormateContext = avformat_alloc_context();
+    if(avformat_open_input(&pFormateContext,input, NULL, NULL )){
+        FFLOGE("%s", "无法打开视频文件");
+        return;
+    }
+    if(avformat_find_stream_info(pFormateContext, NULL) < 0){
+        FFLOGE("%s", "无法获取到视频信息");
+        return;
+    }
+    //获取视频流的索引位置
+    //遍历所有类型的流（音频流、视频流、字幕流），找到视频流
+    int v_stream_idx = -1;
+    int i = 0;
+    for(; i < pFormateContext->nb_streams; i++){
+        if(pFormateContext->streams[i] ->codec ->codec_type == AVMEDIA_TYPE_VIDEO){
+            v_stream_idx = i;
+            break;
+        }
     }
 
-    FFLOGE("解码完成");
+    //只有知道视频的编码方式，才能够根据编码方式去找到解码器
+    //获取视频流中的编解码上下文
+    AVCodecContext *pCodecContxt = pFormateContext->streams[v_stream_idx]->codec;
+    //4.根据编解码上下文中的编码id查找对应的解码
+    AVCodec *pCodec = avcodec_find_decoder(pCodecContxt->codec_id);
+    if(pCodec == NULL){
+        FFLOGE("%", "找不大对应的解码器");
+        return;
+    }
 
-    fclose(fp_yuv);
+    //打开解决码器
+    if(avcodec_open2(pCodecContxt, pCodec, NULL)<0){
+        FFLOGE("%s","解码器无法打开\n");
+        return;
+    }
+    //输出视频信息
+    FFLOGE("视频的文件格式：%s",pFormateContext->iformat->name);
+    FFLOGE("视频时长：%d", (pFormateContext->duration)/1000000);
+    FFLOGE("视频的宽高：%d,%d",pCodecContxt->width,pCodecContxt->height);
+    FFLOGE("解码器的名称：%s",pCodec->name);
 
-    av_frame_free(&pFram);
+    //准备读取
+    //AVPacket用于存储一帧一帧的压缩数据（H264）
+    //缓冲区，开辟空间
+    AVPacket *packet = (AVPacket *) av_malloc(sizeof(AVPacket));;
 
+    //AVFrame用于存储解码后的像素数据(YUV)
+    //内存分配
+    AVFrame *yuv_frame = av_frame_alloc();
+    //YUV420
+    AVFrame *rgb_frame = av_frame_alloc();
+    //只有指定了AVFrame的像素格式、画面大小才能真正分配内存
+    //缓冲区分配内存
+
+    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_Buffer *outBuffer ;
+
+
+    int frame_count = 0;
+    int got_picture, ret;
+    //6.一帧一帧的读取压缩数据
+    while (av_read_frame(pFormateContext, packet) >= 0)
+    {
+        //是视频的 数据
+        if(packet->stream_index == v_stream_idx){
+            ret = avcodec_decode_video2(pCodecContxt,yuv_frame,  &got_picture, packet);
+            if(ret < 0){
+                FFLOGE("%s","解码错误");
+                return;
+            }
+            //为0说明解码完成，非0正在解码
+            if (got_picture)
+            {
+                ANativeWindow_setBuffersGeometry(nativeWindow, pCodecContxt->width, pCodecContxt->height,WINDOW_FORMAT_RGBA_8888);
+                ANativeWindow_lock(nativeWindow, outBuffer, NULL);
+
+                //设置rgb_frame的属性（像素格式、宽高）和缓冲区
+                //rgb_frame缓冲区与outBuffer.bits是同一块内存
+                avpicture_fill((AVPicture *) rgb_frame, (const uint8_t *) outBuffer->bits, AV_PIX_FMT_RGBA, pCodecContxt->width, pCodecContxt->height);
+
+                //YUV->RGBA_8888
+                I420ToARGB(yuv_frame->data[0],yuv_frame->linesize[0],
+                           yuv_frame->data[2],yuv_frame->linesize[2],
+                           yuv_frame->data[1],yuv_frame->linesize[1],
+                           rgb_frame->data[0], rgb_frame->linesize[0],
+                           pCodeCtx->width,pCodeCtx->height);
+
+                ANativeWindow_unlockAndPost(nativeWindow);
+                usleep(1000 * 16);
+            }
+        }
+        av_free_packet(packet);
+    }
+
+    ANativeWindow_release(nativeWindow);
+    av_frame_free(&yuv_frame);
+    av_frame_free(&rgb_frame);
     avcodec_close(pCodecContxt);
-
     avformat_free_context(pFormateContext);
+    env->ReleaseStringUTFChars(input_, input);
+
+
+
+
 
 
     env->ReleaseStringUTFChars(input_, input);
-    env->ReleaseStringUTFChars(output_, output);
 }
--- app/src/main/java/gwj/dev/ffmpeg/MainActivity.java
+++ app/src/main/java/gwj/dev/ffmpeg/MainActivity.java
@@ -7,13 +7,26 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    // Used to load the 'native-lib' library on application startup.
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native void decdoe(String input, String output);
+
+    public native void playVideo(String input, Surface surface);
+
+
+    // Used to load the 'native-lib' library on applica0tion startup.
     static {
         System.loadLibrary("native-lib");
 
@@ -23,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("avutil");
         System.loadLibrary("swresample");
         System.loadLibrary("swscale");
-
     }
 
+    private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,22 +48,9 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
-        findViewById(R.id.sample_change_format).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                String rootPath = Environment.getExternalStorageDirectory().toString();
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String inputPath = "/sdcard/DCIM/Camera/VID_20180428_144437.mp4";
-                        String outputPath = "/sdcard/output_n_yuv420p.yuv";
-                        decdoe(inputPath, outputPath);
-                    }
-                }).start();
-            }
-        });
-
+        videoView = findViewById(R.id.video_view);
+        findViewById(R.id.sample_change_format).setOnClickListener(this);
+        findViewById(R.id.sample_play).setOnClickListener(this);
         requestPermission();
 
     }
@@ -66,12 +66,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 
-    public native void decdoe(String input, String output);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sample_play:
+                String inputPath = "/sdcard/DCIM/Camera/VID_20180428_144437.mp4";
+                Surface surface = videoView.getHolder().getSurface();
+                playVideo(inputPath, surface);
+                break;
+            case R.id.sample_change_format:
+                changeToYuvFormat();
+                break;
+        }
+    }
+
+    /**
+     * 转换格式
+     */
+    private void changeToYuvFormat(){
+        new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String rootPath = Environment.getExternalStorageDirectory().toString();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String inputPath = "/sdcard/DCIM/Camera/VID_20180428_144437.mp4";
+                        String outputPath = "/sdcard/output_n_yuv420p.yuv";
+                        decdoe(inputPath, outputPath);
+                    }
+                }).start();
+            }
+        };
+    }
 
 }
